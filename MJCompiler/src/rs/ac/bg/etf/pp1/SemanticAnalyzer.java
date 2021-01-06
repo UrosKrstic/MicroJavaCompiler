4ba8d37571928ae291b0c70491391270609b60a0
Uros Krstic <uros.krstic98@gmail.com>
2021-01-06 14:08:01 +0100

    src: Implement program insertion into symbol table

--- MJCompiler/src/rs/ac/bg/etf/pp1/SemanticAnalyzer.java
+++ MJCompiler/src/rs/ac/bg/etf/pp1/SemanticAnalyzer.java
@@ -2,6 +2,8 @@ package rs.ac.bg.etf.pp1;
 
 import org.apache.log4j.Logger;
 import rs.ac.bg.etf.pp1.ast.*;
+import rs.etf.pp1.symboltable.Tab;
+import rs.etf.pp1.symboltable.concepts.Obj;
 
 
 public class SemanticAnalyzer extends VisitorAdaptor {
@@ -21,5 +23,15 @@ public class SemanticAnalyzer extends VisitorAdaptor {
 
     public boolean passed(){
     	return !errorDetected;
+    }
+
+    public void visit(ProgName progName) {
+        progName.obj = Tab.insert(Obj.Prog, progName.getProgName(), Tab.noType);
+        Tab.openScope();
+    }
+
+    public void visit(Program program) {
+        Tab.chainLocalSymbols(program.getProgName().obj);
+        Tab.closeScope();
     }
 }
